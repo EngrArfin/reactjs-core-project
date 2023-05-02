ff95345b53280eba7cece674b0a4ef6aa2ce34f5
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2023-05-02 11:18:15 +0600

    increse de and jeson place import data name email

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
- 
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+function ExternalUsers(){
+  const [users, setUsers] = useState([]);
+  // useEffect( ()=>{}, [])
 
-function Counter(){
-  const [count, setCount] = useState(100);
+   useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json)
+    .then(data => setUsers(data))
+   }, [])
+  return(
+    <div>
+      <h2>External Users </h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User> )
+      }
+    </div>
+  )
+}
+function User(props){
+  return(
+    <div style={{border: '2px solid red', margin:'20px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
+// function Counter(){
+//   const [count, setCount] = useState(100);
   
-  const increaseCount =() =>setCount(count + 1);
-  const decreaseCount =() =>setCount(count - 1);
+//   const increaseCount =() =>setCount(count + 1);
+//   const decreaseCount =() =>setCount(count - 1);
 
 
   // const increaseCount =() => {
@@ -23,15 +49,15 @@ function Counter(){
   //       setCount(newCount);
   // }
 
-  return (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={increaseCount}>Increase </button>
-      <button onClick={decreaseCount}>Decrease </button>
+//   return (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={increaseCount}>Increase </button>
+//       <button onClick={decreaseCount}>Decrease </button>
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
 
 // function Counter(){
